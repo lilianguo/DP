9d54e20cef659adcb48ed2578ad7b8a3162c33b9
Rebecca Guo <rebeccaguospecial@gmail.com>
2019-09-08 20:12:38 -0700

    Create JumpGame.java

---
+++ JumpGame.java
@@ -0,0 +1,20 @@
+class JumpGame {
+    public boolean canJump(int[] A) {
+        // write your code here
+        // dp[i] 表示能否到达第i个位置
+        // 什么时候能到达i, 之前只要有一个位置j 满足 j 能到达 && j 到 i的步数 <= j位置最多能跳的步数
+        int n = A.length;
+        boolean[] dp = new boolean[A.length];
+        dp[0] = true;
+        for (int i = 1; i < n; i++) {
+            dp[i] = false;
+            for (int j = 0; j < i; i++) {
+                if (dp[j] && A[j] > (i - j)) {
+                    dp[i] = true;
+                    break;
+                }
+            }
+        }
+        return dp[A.length - 1] == true;
+    }
+}
